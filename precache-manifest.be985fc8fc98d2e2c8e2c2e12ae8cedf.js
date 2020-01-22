ddf17e79ac53510c9d86312eb04f997ccc354ae0
tynsoltys <c.n.soltys@gmail.com>
2020-01-22 16:19:13 -0500

    Updates

---
+++ precache-manifest.be985fc8fc98d2e2c8e2c2e12ae8cedf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e836a4f8dfc3b839f6c5c815aecd930",
+    "url": "/ld-app/index.html"
+  },
+  {
+    "revision": "52377e13a79dba39ad9b",
+    "url": "/ld-app/static/css/main.016ac44d.chunk.css"
+  },
+  {
+    "revision": "849cc085a4c87a241b8f",
+    "url": "/ld-app/static/js/2.d3df49ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ld-app/static/js/2.d3df49ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52377e13a79dba39ad9b",
+    "url": "/ld-app/static/js/main.0b56b624.chunk.js"
+  },
+  {
+    "revision": "9ced5fdff0654177ff00",
+    "url": "/ld-app/static/js/runtime-main.0411dc06.js"
+  }
+]);

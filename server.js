c52a8e93e5606cfd1349795784335759312eada7
tynsoltys <c.n.soltys@gmail.com>
2020-01-22 22:40:54 -0500

    proxy fun

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
+const proxy = require('http-proxy-middleware');
 require('dotenv').config();
 
 // HELLO
@@ -13,25 +14,47 @@ var app = express();
 
 // STUFF THAT'S NEEDED
 app.use(cors());
+app.use(proxy(['http://localhost:9000'], { target: '/' }));
 app.use(bodyParser.json());
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 // production mode
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'));
-});
+//Non api requests in production
+if (
+  process.env.NODE_ENV === 'production' ||
+  process.env.NODE_ENV === 'staging'
+) {
+  // Add production middleware such as redirecting to https
 
-// LISTEN
-app.listen(process.env.PORT || 9000, (req, res) => {
-  console.log(`🎸  You're listening to port 9000`);
-});
+  // Express will serve up production assets i.e. main.js
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // If Express doesn't recognize route serve index.html
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 9000; //Heroku sets port dynamically
+app
+  .listen(PORT, () => {
+    console.log(`🎸  You're listening!`);
+  })
+  .on('error', (err) => {
+    console.log(`Listening Error Code: ${err.code}`);
+  });
 
 // CONNECT DB
 
-mongoose.connect(process.env.MONGODB_URI).catch((error) => handleError(error));
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .catch((error) => handleError(error));
 
 // SCHEMA
 const MsgSchema = new mongoose.Schema({
